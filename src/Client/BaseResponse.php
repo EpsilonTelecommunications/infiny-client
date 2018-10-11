f6a2d21af9884d6ad09bc33ed9d51f45f215b8e4
russellgalpin <floyd26bruno>
2018-10-11 14:24:18 +0100

    Get services

--- src/Client/BaseResponse.php
+++ src/Client/BaseResponse.php
@@ -8,17 +8,27 @@
 namespace Infiny\Client;
 
 
+use Doctrine\Common\Inflector\Inflector;
 use Infiny\Contracts\ApiResponse;
 use Infiny\Contracts\BaseResponse as BaseResponseInterface;
-
 
 class BaseResponse implements BaseResponseInterface
 {
     public function __construct(ApiResponse $response)
     {
         foreach($response->getBody() as $key=>$value) {
-            $studly = str_replace(" ", "", ucwords(str_replace(['-', '_'], ' ', $key)));
-            if(method_exists($this, 'add'.$studly)) {
+
+            $studly = Inflector::classify($key);
+            $studlySingular = Inflector::singularize($studly);
+
+            if(method_exists($this, 'add'.$studlySingular)) {
+                $reflectionClass = new \ReflectionClass(__CLASS__);
+                $parameters = $reflectionClass->getMethod('add'.$studlySingular)->getParameters();
+                $parameterClass = $parameters[0]->getType();
+                $parameter = new $parameterClass($value);
+
+                $this->{'add'.$studlySingular}($parameter);
+            } elseif(method_exists($this, 'add'.$studly)) {
                 $reflectionClass = new \ReflectionClass(__CLASS__);
                 $parameters = $reflectionClass->getMethod('add'.$studly)->getParameters();
                 $parameterClass = $parameters[0]->getType();
