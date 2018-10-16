fc4d1e7f73fd7e966c2b64b23e3422008a6313b2
russellgalpin <floyd26bruno>
2018-10-16 15:30:05 +0100

    Start of adding a service

---
+++ src/Applications/Clx/Requests/Service.php
@@ -0,0 +1,460 @@
+<?php
+/**
+ * User: russ
+ * Date: 16/10/2018
+ * Time: 10:50
+ */
+
+namespace Infiny\Applications\Clx\Requests;
+
+
+use Infiny\Client\BaseRequest;
+
+class Service extends BaseRequest
+{
+    private $name;
+    private $portId;
+    private $servicePortId;
+    private $productId;
+    private $vlan;
+    private $farEndPortId;
+    private $cvlanMappingType;
+    private $vlans = [];
+    private $vlanFrom;
+    private $vlanTo;
+    private $translatedVlan;
+    private $untaggedPortId;
+    private $awsAccount;
+    private $masServiceKey;
+    private $secondaruPortId;
+    private $gcpProjectId;
+    private $hostId;
+    private $linkIp;
+    private $asn;
+    private $md5SessionKey;
+    private $contactName;
+    private $contactEmail;
+    private $contactPhone;
+
+    /**
+     * @return mixed
+     */
+    public function getRequestData()
+    {
+        // TODO: Implement getRequestData() method.
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     * @return Service
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPortId()
+    {
+        return $this->portId;
+    }
+
+    /**
+     * @param mixed $portId
+     * @return Service
+     */
+    public function setPortId($portId)
+    {
+        $this->portId = $portId;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getServicePortId()
+    {
+        return $this->servicePortId;
+    }
+
+    /**
+     * @param mixed $servicePortId
+     * @return Service
+     */
+    public function setServicePortId($servicePortId)
+    {
+        $this->servicePortId = $servicePortId;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProductId()
+    {
+        return $this->productId;
+    }
+
+    /**
+     * @param mixed $productId
+     * @return Service
+     */
+    public function setProductId($productId)
+    {
+        $this->productId = $productId;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVlan()
+    {
+        return $this->vlan;
+    }
+
+    /**
+     * @param mixed $vlan
+     * @return Service
+     */
+    public function setVlan($vlan)
+    {
+        $this->vlan = $vlan;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFarEndPortId()
+    {
+        return $this->farEndPortId;
+    }
+
+    /**
+     * @param mixed $farEndPortId
+     * @return Service
+     */
+    public function setFarEndPortId($farEndPortId)
+    {
+        $this->farEndPortId = $farEndPortId;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCvlanMappingType()
+    {
+        return $this->cvlanMappingType;
+    }
+
+    /**
+     * @param mixed $cvlanMappingType
+     * @return Service
+     */
+    public function setCvlanMappingType($cvlanMappingType)
+    {
+        $this->cvlanMappingType = $cvlanMappingType;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getVlans(): array
+    {
+        return $this->vlans;
+    }
+
+    /**
+     * @param array $vlans
+     * @return Service
+     */
+    public function setVlans(array $vlans): Service
+    {
+        $this->vlans = $vlans;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVlanFrom()
+    {
+        return $this->vlanFrom;
+    }
+
+    /**
+     * @param mixed $vlanFrom
+     * @return Service
+     */
+    public function setVlanFrom($vlanFrom)
+    {
+        $this->vlanFrom = $vlanFrom;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVlanTo()
+    {
+        return $this->vlanTo;
+    }
+
+    /**
+     * @param mixed $vlanTo
+     * @return Service
+     */
+    public function setVlanTo($vlanTo)
+    {
+        $this->vlanTo = $vlanTo;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTranslatedVlan()
+    {
+        return $this->translatedVlan;
+    }
+
+    /**
+     * @param mixed $translatedVlan
+     * @return Service
+     */
+    public function setTranslatedVlan($translatedVlan)
+    {
+        $this->translatedVlan = $translatedVlan;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUntaggedPortId()
+    {
+        return $this->untaggedPortId;
+    }
+
+    /**
+     * @param mixed $untaggedPortId
+     * @return Service
+     */
+    public function setUntaggedPortId($untaggedPortId)
+    {
+        $this->untaggedPortId = $untaggedPortId;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAwsAccount()
+    {
+        return $this->awsAccount;
+    }
+
+    /**
+     * @param mixed $awsAccount
+     * @return Service
+     */
+    public function setAwsAccount($awsAccount)
+    {
+        $this->awsAccount = $awsAccount;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMasServiceKey()
+    {
+        return $this->masServiceKey;
+    }
+
+    /**
+     * @param mixed $masServiceKey
+     * @return Service
+     */
+    public function setMasServiceKey($masServiceKey)
+    {
+        $this->masServiceKey = $masServiceKey;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSecondaruPortId()
+    {
+        return $this->secondaruPortId;
+    }
+
+    /**
+     * @param mixed $secondaruPortId
+     * @return Service
+     */
+    public function setSecondaruPortId($secondaruPortId)
+    {
+        $this->secondaruPortId = $secondaruPortId;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGcpProjectId()
+    {
+        return $this->gcpProjectId;
+    }
+
+    /**
+     * @param mixed $gcpProjectId
+     * @return Service
+     */
+    public function setGcpProjectId($gcpProjectId)
+    {
+        $this->gcpProjectId = $gcpProjectId;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHostId()
+    {
+        return $this->hostId;
+    }
+
+    /**
+     * @param mixed $hostId
+     * @return Service
+     */
+    public function setHostId($hostId)
+    {
+        $this->hostId = $hostId;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLinkIp()
+    {
+        return $this->linkIp;
+    }
+
+    /**
+     * @param mixed $linkIp
+     * @return Service
+     */
+    public function setLinkIp($linkIp)
+    {
+        $this->linkIp = $linkIp;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAsn()
+    {
+        return $this->asn;
+    }
+
+    /**
+     * @param mixed $asn
+     * @return Service
+     */
+    public function setAsn($asn)
+    {
+        $this->asn = $asn;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMd5SessionKey()
+    {
+        return $this->md5SessionKey;
+    }
+
+    /**
+     * @param mixed $md5SessionKey
+     * @return Service
+     */
+    public function setMd5SessionKey($md5SessionKey)
+    {
+        $this->md5SessionKey = $md5SessionKey;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContactName()
+    {
+        return $this->contactName;
+    }
+
+    /**
+     * @param mixed $contactName
+     * @return Service
+     */
+    public function setContactName($contactName)
+    {
+        $this->contactName = $contactName;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContactEmail()
+    {
+        return $this->contactEmail;
+    }
+
+    /**
+     * @param mixed $contactEmail
+     * @return Service
+     */
+    public function setContactEmail($contactEmail)
+    {
+        $this->contactEmail = $contactEmail;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContactPhone()
+    {
+        return $this->contactPhone;
+    }
+
+    /**
+     * @param mixed $contactPhone
+     * @return Service
+     */
+    public function setContactPhone($contactPhone)
+    {
+        $this->contactPhone = $contactPhone;
+        return $this;
+    }
+}
